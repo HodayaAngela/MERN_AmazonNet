96d46ad6838189cc3bba505f9d71cdc3b17b9b41
HodayaAngela <Hodaya.da11@gmail.com>
2023-08-18 16:45:33 +0300

    Home screen

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -56,13 +56,7 @@ const Home = () => {
         ) : (
           <Row>
             {products.map((product) => (
-              <Col
-                key={product.slug}
-                sm={6}
-                md={4}
-                lg={3}
-                className="card mb-3"
-              >
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
                 <Product product={product}></Product>
               </Col>
             ))}
